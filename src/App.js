2b871209952834fcdfb041d1ffb6531f0ee8c171
Rishabhraj001 <rishabhraj1130jnvbgp@gmail.com>
2024-10-12 10:50:49 +0530

    Blog Section complete

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import Dashboard from "./ui/pages/Dashboard";
 import Footer from "./ui/components/Footer";
 import BottomBar from "./ui/components/dashboard/BottomBar";
 
+
 // import Dashboard from "./ui/pages/Dashboard";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import useWindowWidth from "./hooks/useWindowWidth"; // Import the custom hook
 import About from "./ui/pages/About";
+import Blog from "./ui/pages/Blog";
 
 function App() {
   const windowWidth = useWindowWidth(); // Get the current window width
@@ -30,6 +32,14 @@ function App() {
             element={
               <UserDataProvider>
                 <About />
+              </UserDataProvider>
+            }
+          />
+          <Route
+            path="/blogs"
+            element={
+              <UserDataProvider>
+                <Blog />
               </UserDataProvider>
             }
           />
